229bea8200194f590e7408f9dcb579374d5a6c35
thecrazymantha <62051270+thecrazymantha@users.noreply.github.com>
2020-04-22 10:51:59 +0200

    import bootstrap

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
